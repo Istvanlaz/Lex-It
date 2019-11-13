29eb51e6c75c4e47451852170f839ceda70d6fb2
Gauthier Wolters <gauthier.wolters@skynet.be>
2019-11-13 18:34:56 +0100

    first steps landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,25 @@ class PagesController < ApplicationController
   def home
     # write something
   end
+  private
+
+  def resource_name
+    :user
+  end
+  helper_method :resource_name
+
+  def resource
+    @resource ||= User.new
+  end
+  helper_method :resource
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  helper_method :devise_mapping
+
+  def resource_class
+    User
+  end
+  helper_method :resource_class
 end
