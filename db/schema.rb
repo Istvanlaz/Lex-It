620105214a27b9e412592d57eb40a3553039ce03
Gauthier Wolters <gauthier.wolters@skynet.be>
2019-11-14 19:19:45 +0100

    navbar login form and sign up form on landing page

--- db/schema.rb
+++ db/schema.rb
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_134746) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
